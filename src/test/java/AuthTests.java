dfef3021e13371aa14ebf2887a1c4a86a5ca76b9
artur <arturpoluektov@gmail.com>
2023-04-02 15:28:40 +0300

    добавил настройки

--- src/test/java/AuthTests.java
+++ src/test/java/AuthTests.java
@@ -2,10 +2,12 @@ import RequestsBodies.AuthRequest;
 import ResponseBodies.AuthResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
+@Tag("API")
 public class AuthTests {
 
     @Test
